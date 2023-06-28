96237261234b888a2383436af4a4736153d89b05
Ahhj93 <69793084+Ahhj93@users.noreply.github.com>
2023-06-28 22:17:27 +0200

    Update

--- parcoursup_candidats_en_attente.py
+++ parcoursup_candidats_en_attente.py
@@ -1,35 +1,47 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def main():
     # Charger les données à partir du fichier CSV
-    data = pd.read_csv('Parcoursup 2023 - Total.csv', delimiter=';')
+    data = pd.read_csv("Parcoursup 2023 - Total.csv", delimiter=";")
 
     # Extraire les colonnes nécessaires
-    dates = pd.to_datetime(data['Date'], format='%d/%m')
-    en_attente = data["Candidats n'ayant pas encore reçu de proposition ou en attente de place"]
+    dates = pd.to_datetime(data["Date"], format="%d/%m")
+    en_attente = data[
+        "Candidats n'ayant pas encore reçu de proposition ou en attente de place"
+    ]
 
     # Créer les limites des bacs pour l'histogramme en escalier
-    x = range(len(dates)+1)
+    x = range(len(dates) + 1)
     y = list(en_attente) + [en_attente.iloc[-1]]  # Candidats en attente
 
     # Créer le graphique avec la courbe en rouge pour les candidats en attente
     plt.figure(figsize=(8, 6))
-    plt.step(x, y, where='post', color='orange', linewidth=2, alpha=0.7)
-    plt.xlabel('Date')
-    plt.ylabel("Candidats n'ayant pas encore reçu de\nproposition ou en attente de place")
-    plt.title("Évolution en fonction du temps du nombre de\ncandidats sans affectation sur Parcoursup en 2023")
+    plt.step(x, y, where="post", color="orange", linewidth=2, alpha=0.7)
+    plt.xlabel("Date")
+    plt.ylabel(
+        "Candidats n'ayant pas encore reçu de\nproposition ou en attente de place"
+    )
+    plt.title(
+        "Évolution en fonction du temps du nombre de\ncandidats sans affectation sur Parcoursup en 2023"
+    )
     x_ticks = range(0, len(dates), 3)
-    x_labels = dates[x_ticks].dt.strftime('%d/%m')
+    x_labels = dates[x_ticks].dt.strftime("%d/%m") # type: ignore
     plt.xticks(x_ticks, x_labels, rotation=45)
     plt.ylim(0, 400000)
     plt.twinx()
-    plt.ylim(0, (400000/827271)*100)
-    plt.ylabel('Pourcentage en fonction du Nombre de candidats')
+    plt.ylim(0, (400000 / 827271) * 100)
+    plt.ylabel("Pourcentage en fonction du Nombre de candidats")
     plt.tight_layout()
-    plt.savefig("Évolution en fonction du temps du nombre de candidats sans affectation sur Parcoursup en 2023.svg")
-    print("Le graphique a été enregistré dans le fichier 'Évolution en fonction du temps du nombre de candidats sans affectation sur Parcoursup en 2023.svg'.")
+    plt.savefig(
+        "Évolution en fonction du temps du nombre de candidats sans affectation sur Parcoursup en 2023.svg"
+    )
+    print(
+        "Le graphique a été enregistré dans le fichier 'Évolution en fonction du temps du nombre de candidats sans affectation sur Parcoursup en 2023.svg'."
+    )
     plt.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- parcoursup_evolution.py
+++ parcoursup_evolution.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def main():
     # Charger les données à partir du fichier CSV
-    data = pd.read_csv('Parcoursup 2023 - Total.csv', delimiter=';')
+    data = pd.read_csv("Parcoursup 2023 - Total.csv", delimiter=";")
 
     # Extraire les colonnes nécessaires
-    dates = pd.to_datetime(data['Date'], format='%d/%m')
-    recus = data["Candidats ayant reçu une ou plusieurs propositions d'admission"].fillna(0)
-    quittes = data["Candidats ayant quitté la plateforme avant de recevoir une proposition d'admission"].fillna(0)
-    non_quittes = data["Candidats n'ayant pas encore reçu de proposition ou en attente de place n'ayant pas quitté la plateforme"].fillna(0)
+    dates = pd.to_datetime(data["Date"], format="%d/%m")
+    recus = data[
+        "Candidats ayant reçu une ou plusieurs propositions d'admission"
+    ].fillna(0)
+    quittes = data[
+        "Candidats ayant quitté la plateforme avant de recevoir une proposition d'admission"
+    ].fillna(0)
+    non_quittes = data[
+        "Candidats n'ayant pas encore reçu de proposition ou en attente de place n'ayant pas quitté la plateforme"
+    ].fillna(0)
 
     # Créer les limites des bacs pour l'histogramme empilé
     x = range(len(dates))
@@ -18,18 +25,29 @@ def main():
     plt.figure(figsize=(8, 6))
 
     # Tracer l'histogramme empilé avec des barres collées
-    plt.bar(x, recus, width=1, color='green', label="Candidats admis")
-    plt.bar(x, quittes, width=1, bottom=recus, color='red', label="Candidats ayant quitté")
-    plt.bar(x, non_quittes, width=1, bottom=recus + quittes, color='orange', label="Candidats en attente")
+    plt.bar(x, recus, width=1, color="green", label="Candidats admis")
+    plt.bar(
+        x, quittes, width=1, bottom=recus, color="red", label="Candidats ayant quitté"
+    )
+    plt.bar(
+        x,
+        non_quittes,
+        width=1,
+        bottom=recus + quittes,
+        color="orange",
+        label="Candidats en attente",
+    )
 
     # Ajouter les étiquettes et les titres
-    plt.xlabel('Date')
-    plt.ylabel('Nombre de candidats')
-    plt.title("Évolution en fonction du temps du statut\ndes candidats sur Parcoursup en 2023")
+    plt.xlabel("Date")
+    plt.ylabel("Nombre de candidats")
+    plt.title(
+        "Évolution en fonction du temps du statut\ndes candidats sur Parcoursup en 2023"
+    )
 
     # Simplifier les étiquettes des dates
     x_ticks = range(0, len(dates), 3)
-    x_labels = dates[x_ticks].dt.strftime('%d/%m')
+    x_labels = dates[x_ticks].dt.strftime("%d/%m") # type: ignore
 
     # Définir les positions et les étiquettes des ticks sur l'axe des x
     plt.xticks(x_ticks, x_labels, rotation=45)
@@ -48,13 +66,18 @@ def main():
     # Définir les limites des axes y pour le pourcentage
     plt.ylim(0, 100)
     # Ajouter l'étiquette pour l'axe y du pourcentage
-    plt.ylabel('Pourcentage en fonction du Nombre de candidats')
+    plt.ylabel("Pourcentage en fonction du Nombre de candidats")
 
     # Afficher le graphique
     plt.tight_layout()
-    plt.savefig("Évolution en fonction du temps du statut des candidats sur Parcoursup en 2023.svg")
-    print("Le graphique a été enregistré dans le fichier 'Évolution en fonction du temps du statut des candidats sur Parcoursup en 2023'.")
+    plt.savefig(
+        "Évolution en fonction du temps du statut des candidats sur Parcoursup en 2023.svg"
+    )
+    print(
+        "Le graphique a été enregistré dans le fichier 'Évolution en fonction du temps du statut des candidats sur Parcoursup en 2023'."
+    )
     plt.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
